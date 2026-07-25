4ed23797f22adbfb9d59ea3af298049043e2be64
manoj kumar m g <manojmanu4530@gmail.com>
2026-07-25 20:47:30 +0530

    Complete JARVIS v0.2.0 command engine

--- app/core/jarvis.py
+++ app/core/jarvis.py
@@ -1,4 +1,4 @@
-from app.utils.logger import logger 
+from datetime import datetime
 
 class Jarvis:
     def __init__(self):
@@ -6,10 +6,52 @@ class Jarvis:
         self.version="0.2.0"
 
     def start(self):
-        logger.info("Jarvis has started")
+    
 
         print("="*50)
         print(f"Hello, I am {self.name}")
-        print("Your AI Assistent is now online.")
         print(f"Version :{self.version}")
         print("="*50)
+
+        while True:
+            command=input("\nJarvis>").strip().lower()
+
+            if command == "help":
+                self.show_help()
+
+            elif command=="version":
+                print(f"current version :{self.version}")
+
+            elif command=="time":
+                current_time=datetime.now().strftime("%I:%M:%S %p")
+                print(f"current Time: {current_time}")
+
+            elif command=="date":
+                current_date=datetime.now().strftime("%d-%m-%y")
+                print(f"Today's date: {current_date}")
+
+            elif command=="hello":
+                print("Hello how can I help you?")
+
+            elif command=="exit":
+                self.shutdown()
+                return False
+            
+            else:
+                print("Sorry,I don't understand that command.")
+                return True
+            
+      
+      
+    def show_help(self):
+        print("\nAvailable commands")
+        print("---------------------")
+        print("help")
+        print("vesion")
+        print("time")
+        print("date")
+        print("hello")
+        print("exit")
+
+    def shutdown(self):
+        print("Goodbye : shutting down JARVIS....") 
